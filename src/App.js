77e662cd1d0f7a767a0df6fa81595b0b728e9feb
Andy Ju <andy.ju@duke.edu>
2018-11-18 12:14:15 -0500

    this is nice

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Search from './Search.js';
 import logo from './IMG_1039.PNG';
 import './App.css';
 import Map from './components/Map.js'
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
+import {withStyles} from '@material-ui/core/styles';
+// import AppBar from '@material-ui/core/AppBar';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  appHeader:{
+    backgroundColor: ''
+  },
+  searchBar:{
+    marginTop: '16px'
+  }
+});
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" height={30}/>
-        </header>
-        <Search/>
-        <Map/>
-      </div>
-    );
+    const {classes} = this.props;
+
+    return (<div className="App">
+      <header className={classes.appHeader}>
+        <Grid container="container" spacing={24}>
+          <Grid item="item" spacing={4}>
+            <img src={logo} className="App-logo" alt="logo" height={30}/>
+          </Grid>
+          <Grid item="item" spacing={18}>
+            <Search clasName={classes.searchBar}/>
+          </Grid>
+        </Grid>
+      </header>
+      <Map/>
+    </div>);
   }
 }
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(App);
--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,40 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
-class Search extends Component {
+
+const styles = theme => ({
+  container: {
+    //display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '12%'
+  },
+  address: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '35%'
+  },
+  submit:{
+    marginTop: 16
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+class Search extends React.Component {
   state = {
     query: ''
   }
@@ -12,16 +46,26 @@ class Search extends Component {
   handleSubmit = () => {}
 
   render() {
-    return (<form onSubmit={this.handleSubmit}>
-      <input placeholder="Find your Boxing Match" ref={input => this.search += " " + input}/>
-      <input placeholder="Length (ft)" type="number" ref={input => this.search += " " + input}/>
-      <input placeholder="Width (ft)" type="number" ref={input => this.search += " " + input}/>
-      <input placeholder="Height (ft)" type="number" ref={input => this.search += " " + input}/>
-      <input type="submit" value="Submit"/>
-        <p>{this.state.query}</p>
-      </form>
-      )
-      }
-}
+    const { classes } = this.props;
+    return (
+      <form className={classes.container} onSubmit={this.handleSubmit}>
+      <TextField label="Location" className={classes.address} variant="outlined" margin="normal" ref={input => this.search += " " + input}/>
+      <TextField label="Length"  InputProps={{
+            startAdornment: <InputAdornment position="start">Feet</InputAdornment>,
+          }}className={classes.textField}variant="outlined" margin="normal" type="number" ref={input => this.search += " " + input}/>
+      <TextField label="Width" InputProps={{
+            startAdornment: <InputAdornment position="start">Feet</InputAdornment>,
+          }}className={classes.textField}variant="outlined" margin="normal" type="number" ref={input => this.search += " " + input}/>
+      <TextField label="Height" InputProps={{
+            startAdornment: <InputAdornment position="start">Feet</InputAdornment>,
+          }}className={classes.textField} variant="outlined" margin="normal" type="number" ref={input => this.search += " " + input}/>
+        <Button variant="outlined" className={classes.submit} type="submit" color="primary" value="Submit">Search</Button>
+    </form>)
 
-export default Search
+  }
+}
+Search.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Search);
